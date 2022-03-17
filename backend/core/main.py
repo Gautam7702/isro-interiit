cd345ffe38cf6a04d6380899e1a9ef3579d525ea
debjit <debjitbhowal.db@gmail.com>
2022-03-17 09:47:56 +0530

    array trimming edge case included

--- backend/core/main.py
+++ backend/core/main.py
@@ -1,6 +1,16 @@
 from backend.core.curve_completion import complete_curve
 from backend.core.smoothen_curve import moving_average
 from backend.core.peak_information import get_peak_information
+from backend.utils import vars
+
+def trim_graph(array: list) -> list:
+    """
+    Trim graph to the right.
+    """
+    if len(array) > 2 * vars.array_trim:
+        return array[vars.array_trim:-vars.array_trim]
+    else:
+        return array
 
 
 def get_response(matrix) -> dict:
@@ -8,7 +18,8 @@ def get_response(matrix) -> dict:
     Get response from data.
     """
     array = complete_curve(matrix)
-    array = moving_average(array, 300)[500:-500]
+    array = moving_average(array, 300)
+    array = trim_graph(array)
     peaks_info = get_peak_information(array)
 
     return {
