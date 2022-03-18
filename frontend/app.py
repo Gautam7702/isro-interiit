10693902fc6bc397618d88c0477a873784ba5847
Gautam7702 <gomcisethia@gmail.com>
2022-03-18 23:05:54 +0530

    final commit

--- frontend/app.py
+++ frontend/app.py
@@ -1,31 +1,50 @@
-from flask import Flask, render_template,request,redirect,jsonify
+from xml.dom.minidom import Document
+from flask import Flask, render_template,request,redirect,jsonify,flash
 import os
-import json
 import sys
-sys.path.insert(1, 'C:\\Users\\gomci\\OneDrive\\Desktop\\isro_final\\isro-interiit')    
+cwd = os.getcwd()
+sys.path.insert(1, cwd)  
 from backend.main import get_analysis_data
-    
-
 app = Flask(__name__)
 UPLOADFOLDER = 'frontend\\uploads'
+def uploadFile(FILETYPE):
+    if(request.method == 'POST'):
+        if 'file' not in request.files :
+            return redirect('/')
+        file = request.files['file']
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        path = "frontend\\uploads\\"+filename 
+        f = get_analysis_data(path,FILETYPE)
+        os.remove("frontend\\uploads\\"+filename)
+        if(f['OK']['status']==False):
+           return render_template('results.html',graphData = f,error=1)
+        return render_template('results.html',graphData = f,error=0)
+
 app.config['UPLOAD_FOLDER'] = UPLOADFOLDER
 @app.route('/')
 def homePage():
     return render_template('index.html')
 
-@app.route('/submit',methods=['GET','POST'])
-def uploadFile():
-    if(request.method == 'POST'):
-        if 'file' not in request.files :
-            return redirect('/')
-        print(request)
-        file = request.files['file']
-        filename = file.filename
-        # print(file)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        path = "frontend/uploads/"+filename 
-        f = get_analysis_data(path,"fits")
-        # print(f)
-        return render_template('results.html',graphData = f)
+@app.route('/submitFITS',methods=['GET','POST'])
+def fits():
+    return uploadFile('fits')
+
+@app.route('/submitASCII',methods=['GET','POST'])
+def ascii():
+    return uploadFile('ascii')
+
+@app.route('/submitCDF',methods=['GET','POST'])
+def cdf():
+    return uploadFile('cdf')
+
+@app.route('/submitXLS',methods=['GET','POST'])
+def xls():
+    return uploadFile('xls')
+
+@app.route('/documentation')
+def showDoc():
+    return render_template('documentation.html')
 if(__name__=='__main__'):
     app.run(debug=True)
+
